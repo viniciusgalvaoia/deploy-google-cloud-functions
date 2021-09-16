9526add127205612cc22824d4ff3b3be7dec51a0
unknown <vinicius.galvao@passeidireto.com>
2021-09-16 00:19:27 -0300

    Fixinig cloud functions name

--- deploy_multiple_functions_script.py
+++ deploy_multiple_functions_script.py
@@ -1,16 +1,16 @@
 import subprocess
 
 source_dir_and_functions_dict = {
-    "C:\humane\\talentcards\cloud_functions\\groups_to_bq": "groups_to_bq",
-    "C:\humane\\talentcards\cloud_functions\\groups_to_landing_zone": "groups_to_landing_zone",
-    "C:\humane\\talentcards\cloud_functions\\reports_to_bq": "reports_to_bq",
-    "C:\humane\\talentcards\cloud_functions\\reports_to_landing_zone": "reports_to_landing_zone",
-    "C:\humane\\talentcards\cloud_functions\\sets_to_bq": "sets_to_bq",
-    "C:\humane\\talentcards\cloud_functions\\sets_to_landing_zone": "sets_to_landing_zone",
-    "C:\humane\\talentcards\cloud_functions\\users_engagement_to_bq": "users_engagement_to_bq",
-    "C:\humane\\talentcards\cloud_functions\\users_to_bq": "users_to_bq",
-    "C:\humane\\talentcards\cloud_functions\\users_to_landing_zone": "users_to_landing_zone",
-    "C:\humane\\talentcards\cloud_functions\\talentcards-manual-users-extraction-to-gbq": "manual-users-extraction-to-gbq",
+    "C:\humane\\talentcards\cloud_functions\\groups_to_bq": "talentcard-groups-to-bq",
+    "C:\humane\\talentcards\cloud_functions\\groups_to_landing_zone": "talentcard-groups-to-landing-zone",
+    "C:\humane\\talentcards\cloud_functions\\reports_to_bq": "talentcard-reports-to-bq",
+    "C:\humane\\talentcards\cloud_functions\\reports_to_landing_zone": "talentcard-reports-to-landing-zone",
+    "C:\humane\\talentcards\cloud_functions\\sets_to_bq": "talentcard-sets-to-bq",
+    "C:\humane\\talentcards\cloud_functions\\sets_to_landing_zone": "talentcard-stalentcard-sets-to-landing-zone",
+    "C:\humane\\talentcards\cloud_functions\\users_engagement_to_bq": "talentcard-users-engagement-to-bq",
+    "C:\humane\\talentcards\cloud_functions\\users_to_bq": "talentcard-users-to-bq",
+    "C:\humane\\talentcards\cloud_functions\\users_to_landing_zone": "talentcard-users-to-landing-zone",
+    "C:\humane\\talentcards\cloud_functions\\talentcards-manual-users-extraction-to-gbq": "talentcards-manual-users-extraction-to-gbq",
 }
 
 for source_dir, function_name in source_dir_and_functions_dict.items():
